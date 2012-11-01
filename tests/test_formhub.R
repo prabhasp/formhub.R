b9e91577a2a857528ade99ef781976a716b4b58b
Prabhas Pokharel <prabhas.pokharel@gmail.com>
2012-11-01 18:23:54 -0400

    tests failing because i"m re-creating the data-frame; figure out how not to

--- tests/test_formhub.R
+++ tests/test_formhub.R
@@ -1,6 +1,10 @@
 library(testthat)
-# setwd("~/Code/nga_cleaning_scripts/")
-test_dir("tests/")
+
+source("~/Code/nga_cleaning_scripts/formhub.R")
+# test_dir("~/Code/nga_cleaning_scripts/tests/")
+# setwd("~/Code/nga_cleaning_scripts/tests/")
+
+
 edu_datafile <- "fixtures/edu1.csv"
 edu_schemafile <- "fixtures/edu1.json"
 extraSchema = setNames(data.frame(rbind(
@@ -8,6 +12,14 @@ extraSchema = setNames(data.frame(rbind(
                     c("mylga_state", "select one", "State"))),
                        c("name", "type", "label"))
 edu_df <- formhubRead(edu_datafile, edu_schemafile, extraSchema=extraSchema)
+edu_rawdf <- read.csv(edu_datafile, na.strings="n/a", stringsAsFactors=FALSE, header=TRUE)
+
+test_that("reCastingRVectors Works as expected", {
+  expect_true(is.character(edu_rawdf$mylga))
+  expect_true(is.factor(recastRVectorBasedOnFormhubType(edu_rawdf$mylga, "select one")))
+  expect_true(is.character(recastRVectorBasedOnFormhubType(edu_rawdf$ward, "text")))
+})
+
 
 test_that("formhubRead converted types properly", {
   expect_equal(1, 1)
@@ -15,4 +27,6 @@ test_that("formhubRead converted types properly", {
   expect_true(is.factor(edu_df$mylga))
   expect_true(is.factor(edu_df$mylga_state))
   expect_true(is.numeric(edu_df$num_students_total_gender.num_students_female))
+  expect_true(is.character(edu_df$ward))
+  expect_true(is.character(edu_df$community))
 })
