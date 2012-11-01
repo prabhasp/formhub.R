61434d947be435494b5aea89e82ad27e9cb666a9
Salah <salahchafik@gmail.com>
2012-11-01 17:15:44 -0400

    updates to script

--- CleanOut999s.R
+++ CleanOut999s.R
@@ -4,9 +4,10 @@ source("scripts/formhub.R")
 library(stringr)
 
 merged_education <- read.csv("in_process_data/merged/Education_661_Merged.csv",
-                          header=TRUE, na.strings=c('999', '9999', '99999', '999999'))
+                          header=TRUE, na.strings=c('999', '9999', '99999', '999999' , 'n/a'))
 merged_health <- read.csv("in_process_data/merged/Health_661_Merged.csv",
-                          header=TRUE, na.strings=c('999', '9999', '99999', '999999'))
+                          header=TRUE, na.strings=c('999'))
 merged_water <- read.csv("in_process_data/merged/Water_661_Merged.csv")
+
 
 # merged_education2 <- subset(merged_education, subset=(as.numeric(as.character(num_benches)) > 10000))
